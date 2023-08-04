d666c974e9568002a39a11cf454c435bbe0a1208
johnnyalvesx <alvesjohnny@outlook.com>
2023-08-04 13:36:02 -0300

    Tratamento de exception na busca de filme na API com try-catch

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -33,8 +33,14 @@ public class PrincipalComBusca {
         Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
         TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
         System.out.println(meuTituloOmdb);
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("Título já convertido");
-        System.out.println(meuTitulo);
+
+        try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Título já convertido");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e) {
+            System.out.println("Houve um erro: ");
+            System.out.println(e.getMessage());
+        }
     }
 }
