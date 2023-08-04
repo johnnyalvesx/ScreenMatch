bd0f7e55c73be29f1cd7883f190c4e868ea71043
johnnyalvesx <alvesjohnny@outlook.com>
2023-08-03 23:16:46 -0300

    Integração da API OMDb utilizando HttpClient, HttpRequest e HttpResponse

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Digite o nome do filme que deseja buscar: ");
+        var busca = in.nextLine();
+        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=f44b1e39";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=constantine&apikey=f44b1e39"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
